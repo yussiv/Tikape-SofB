84f2985477ffbd60011f859c6e8d573589ac9af1
Kari Vänttinen <kvanttinen@gmail.com>
2016-10-07 22:48:38 +0300

    ajan näytön viilausta

--- src/main/java/tikape/runko/Formatteri.java
+++ src/main/java/tikape/runko/Formatteri.java
@@ -3,6 +3,8 @@ package tikape.runko;
 import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Formatteri {
 
@@ -16,17 +18,36 @@ public class Formatteri {
 
     public Formatteri() {
         this.stringToDateFormat = "yyyy-MM-dd hh:mm:ss";
-        this.dateToStringFormat = "dd.MM.yyyy hh:mm";
+        this.dateToStringFormat = "dd.MM.yyyy HH:mm";
     }
 
     public String formatoi(String date) {
         dateString = "";
         try {
             SimpleDateFormat sdf1 = new SimpleDateFormat(stringToDateFormat);
-            Date pvm = sdf1.parse(date);
-            SimpleDateFormat sdf2 = new SimpleDateFormat(dateToStringFormat);
-            dateString = sdf2.format(pvm);
+            Date dateTimeGMT = sdf1.parse(date);
+
+            Calendar postausAika = new GregorianCalendar();
+            postausAika.setTime(dateTimeGMT);
+            postausAika.add(10, 3);
+
+            Calendar aikaNyt = new GregorianCalendar();
+            Date aikaNytDate = new Date();
+            aikaNytDate.getTime();
+            aikaNyt.setTime(aikaNytDate);
+            aikaNyt.set(10, 3);
+
+            if (aikaNyt.get(Calendar.YEAR) == postausAika.get(Calendar.YEAR) && aikaNyt.get(Calendar.MONTH) == postausAika.get(Calendar.MONTH) && aikaNyt.get(Calendar.DAY_OF_MONTH) == postausAika.get(Calendar.DAY_OF_MONTH)) {
+                dateString = "Tänään kello " + postausAika.get(11) + "." + postausAika.get(12);
+            } else if(aikaNyt.get(Calendar.YEAR) == postausAika.get(Calendar.YEAR) && aikaNyt.get(Calendar.MONTH) == postausAika.get(Calendar.MONTH) && aikaNyt.get(Calendar.DAY_OF_MONTH) - postausAika.get(Calendar.DAY_OF_MONTH) == 1) {
+                dateString = "Eilen kello " + postausAika.get(11) + "." + postausAika.get(12);
+            } else {
+                Date postausAikaDate = postausAika.getTime();
+                SimpleDateFormat sdf2 = new SimpleDateFormat(dateToStringFormat);
+                dateString = sdf2.format(postausAikaDate);
+            }
             return dateString;
+            
         } catch (ParseException e) {
             return "";
         }
