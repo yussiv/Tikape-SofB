e47ca52d6d06743ca1bc5df770a15ad23f08c00d
Kari Vänttinen <kvanttinen@gmail.com>
2016-10-15 02:24:20 +0300

    formatterin fiksausta

--- src/main/java/tikape/runko/util/Formatteri.java
+++ src/main/java/tikape/runko/util/Formatteri.java
@@ -29,39 +29,39 @@ public final class Formatteri {
             Date aikaNytDate = new Date();
             aikaNytDate.getTime();
             aikaNyt.setTime(aikaNytDate);
-            aikaNyt.set(10, 3);
-            return aikaNyt.getTime().toString();
+            aikaNyt.set(10, 11);
+//            return aikaNyt.getTime().toString();
 
-//            if (aikaNyt.get(Calendar.YEAR) == postausAika.get(Calendar.YEAR) && aikaNyt.get(Calendar.MONTH) == postausAika.get(Calendar.MONTH) && aikaNyt.get(Calendar.DAY_OF_MONTH) + 1 == postausAika.get(Calendar.DAY_OF_MONTH)) {
-//                dateString = "Tänään kello ";
-//                if (postausAika.get(11) < 10) {
-//                    dateString += "0" + postausAika.get(11) + ".";
-//                } else {
-//                    dateString += "" + postausAika.get(11) + ".";   
-//                }
-//                if (postausAika.get(12) < 10) {
-//                    dateString += "0" +  postausAika.get(12);
-//                } else {
-//                    dateString += postausAika.get(12);
-//                }
-//            } else if(aikaNyt.get(Calendar.YEAR) == postausAika.get(Calendar.YEAR) && aikaNyt.get(Calendar.MONTH) == postausAika.get(Calendar.MONTH) && aikaNyt.get(Calendar.DAY_OF_MONTH) - postausAika.get(Calendar.DAY_OF_MONTH) == 1) {
-//                dateString = "Eilen kello ";
-//                if (postausAika.get(11) < 10) {
-//                    dateString += "0" + postausAika.get(11) + ".";
-//                } else {
-//                    dateString += "" + postausAika.get(11) + ".";   
-//                }
-//                if (postausAika.get(12) < 10) {
-//                    dateString += "0" +  postausAika.get(12);
-//                } else {
-//                    dateString += postausAika.get(12);
-//                }
-//            } else {
-//                Date postausAikaDate = postausAika.getTime();
-//                SimpleDateFormat sdf2 = new SimpleDateFormat(dateToStringFormat);
-//                dateString = sdf2.format(postausAikaDate);
-//            }
-//            return dateString;
+            if (aikaNyt.get(Calendar.YEAR) == postausAika.get(Calendar.YEAR) && aikaNyt.get(Calendar.MONTH) == postausAika.get(Calendar.MONTH) && aikaNyt.get(Calendar.DAY_OF_MONTH) == postausAika.get(Calendar.DAY_OF_MONTH)) {
+                dateString = "Tänään kello ";
+                if (postausAika.get(11) < 10) {
+                    dateString += "0" + postausAika.get(11) + ".";
+                } else {
+                    dateString += "" + postausAika.get(11) + ".";   
+                }
+                if (postausAika.get(12) < 10) {
+                    dateString += "0" +  postausAika.get(12);
+                } else {
+                    dateString += postausAika.get(12);
+                }
+            } else if(aikaNyt.get(Calendar.YEAR) == postausAika.get(Calendar.YEAR) && aikaNyt.get(Calendar.MONTH) == postausAika.get(Calendar.MONTH) && aikaNyt.get(Calendar.DAY_OF_MONTH) - postausAika.get(Calendar.DAY_OF_MONTH) == 1) {
+                dateString = "Eilen kello ";
+                if (postausAika.get(11) < 10) {
+                    dateString += "0" + postausAika.get(11) + ".";
+                } else {
+                    dateString += "" + postausAika.get(11) + ".";   
+                }
+                if (postausAika.get(12) < 10) {
+                    dateString += "0" +  postausAika.get(12);
+                } else {
+                    dateString += postausAika.get(12);
+                }
+            } else {
+                Date postausAikaDate = postausAika.getTime();
+                SimpleDateFormat sdf2 = new SimpleDateFormat(dateToStringFormat);
+                dateString = sdf2.format(postausAikaDate);
+            }
+            return dateString;
             
         } catch (Exception e) { // if all else fails, do nothing
             return "";
