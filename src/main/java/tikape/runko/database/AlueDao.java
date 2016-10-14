7005c80b23595aa4d1c0c14c711cf77f1d61b2a1
Kari Vänttinen <kvanttinen@gmail.com>
2016-10-15 01:31:02 +0300

    pieni heroku fix

--- src/main/java/tikape/runko/database/AlueDao.java
+++ src/main/java/tikape/runko/database/AlueDao.java
@@ -79,7 +79,8 @@ public class AlueDao implements Dao<Alue, Integer> {
                     rs.getInt("id"),
                     rs.getString("nimi"),
                     rs.getInt("viestit"),
-                    Formatteri.formatoi(rs.getString("timestamp"))
+//                    Formatteri.formatoi(rs.getString("timestamp"))
+                    rs.getString("timestamp")
                 )
         );
     }
